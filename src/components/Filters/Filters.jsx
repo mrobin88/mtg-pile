418bff644ebfb02cb170675e16ae807231464948
GA SF <gasf@GAs-MacBook-Air.local>
2019-10-14 09:03:20 -0700

    added file for RM.MD

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -19,27 +19,31 @@ class Filter extends Component {
             <section>
                 <input
                 className={styles.nameSearch}
-                placeholder="contains or is named"
+                placeholder="Name"
                 name="name"
                 value={this.name} 
                 onChange={this.props.handleFilterChange}
                 />
                 <select className={styles.fSelect} name="setName" onChange={this.props.handleFilterChange}>
+                <option value="" disabled selected>SET</option>
                 {mtgSets.map(sname =><option value={sname} >{sname}</option>)}</select>
                 
                 <select className={styles.fSelect} name = "type" onChange={this.props.handleFilterChange}>
+                <option value="" disabled selected>TYPE</option>
                 {mtgTypes.map(type =><option value={type} >{type}</option>)}</select>
 
                 <select className={styles.fSelect} name="colors" onChange={this.props.handleFilterChange}>
+                <option value="" disabled selected>COLOR</option>
                 {colors.map(col =><option value={[col]}>{col}</option>)}</select>
                 
                 <select className={styles.fSelect} name="rarity"onChange={this.props.handleFilterChange}>
+                <option value="" disabled selected>RARITY</option>
                 {rarity.map(rare =><option value={rare}>{rare}</option>)}</select>
                 <div className={styles.filterBtns}>
                 <form  onSubmit={this.props.handleFilterSubmit}>
                     <button className={styles.searchBtn} type="submit">Search</button>
                 </form>
-                <button className={styles.searchBtn} onClick={this.props.resetFilter}>Clear</button>
+                <button className={styles.searchBtn} onClick={() => window.location.reload(false).then(this.props.resetFilter)}>Clear</button>
                 </div>
             </section>
         )
